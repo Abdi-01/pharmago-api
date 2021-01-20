8dc36f16f01ffce2fffd89c5bc59867c5139a854
Alex Kurniawan <64827057+alexkurniawan-ng@users.noreply.github.com>
2021-01-21 04:37:23 +0700

    '20jan'

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -2,8 +2,8 @@ const pool = require('../db')
 
 module.exports = ({
     getProducts: (req, res) => {
-        console.log('getProducts req.query: ', req.query.idproduct)
-        console.log('getProducts req.query: ', req.query.category)
+        console.log('getProducts req.query.idproduct: ', req.query.idproduct)
+        console.log('getProducts req.query.category: ', req.query.category)
 
         let sqlJoin = `tbp.*, tbc.* FROM tbproduct tbp JOIN tbproduct_category tbpc ON tbp.idproduct = tbpc.idproduct
                         JOIN tbcategory tbc ON tbc.idcategory = tbpc.idcategory`
@@ -12,7 +12,7 @@ module.exports = ({
         if (req.query.idproduct) {
             sqlGet = `SELECT ${sqlJoin} WHERE tbp.idproduct = ${req.query.idproduct};`
         } else if (req.query.category) {
-            sqlGet = `SELECT ${sqlJoin} WHERE tbc.category = ${req.query.category};`
+            sqlGet = `SELECT ${sqlJoin} WHERE tbc.category = ${pool.escape(req.query.category)};`
         } else {
             sqlGet = `SELECT ${sqlJoin};`
         }
@@ -22,6 +22,15 @@ module.exports = ({
             if (err) res.status(500).send(err)
 
             res.status(200).send({products: results})
+        })
+    },
+    getCategory: (req, res) => {
+        let sqlGet = `SELECT * FROM tbcategory;`
+
+        pool.query(sqlGet, (err, results) => {
+            if (err) res.status(500).send(err)
+
+            res.status(200).send({category: results})
         })
     }
 })
--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const { productsController } = require('../controllers')
 
 router.get('/', productsController.getProducts)
+router.get('/category', productsController.getCategory)
 
 module.exports = router;
